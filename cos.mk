09bd6a438cec3d7f274b90e1211a785e602c5808
Diazz72 <balkan.clash@gmail.com>
2017-03-29 22:27:25 +0200

    initial push

---
+++ cos.mk
@@ -0,0 +1,35 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit some common CosmicOS stuff.
+$(call inherit-product, vendor/cos/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := aosp_angler
+PRODUCT_DEVICE := angler
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 6P
+PRODUCT_MANUFACTURER := Huawei
+PRODUCT_RESTRICT_VENDOR_FILES := true
+
+#Official Builds Tag
+COS_RELEASE=true
+
+#Don't ship SuperSU
+#WITH_SUPERSU := false
+
+#Ship snap cam && cam 2
+#PRODUCT_PACKAGES += \
+#    Camera2 \
+#    Snap
+
+# Use the latest approved GMS identifiers unless running a signed build
+#ifneq ($(SIGN_BUILD),true)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=google/angler/angler:7.1.1/NUF26N/3687344:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.1.1 NUF26N 3687344 release-keys"
+#endif
