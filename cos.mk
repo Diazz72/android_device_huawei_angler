1ce44dc730a3d53b0d5ea8579411074a8f75e8a5
Diazz72 <balkan.clash@gmail.com>
2017-04-05 18:44:18 +0200

    PRODUCT_NAME misspell fix

--- cos.mk
+++ cos.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cos/common.mk)
 $(call inherit-product, device/huawei/angler/aosp_angler.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_angler
+PRODUCT_NAME := cos_angler
 PRODUCT_DEVICE := angler
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 6P
